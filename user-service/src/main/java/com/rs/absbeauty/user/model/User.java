f7ec52b1b43f471781dcd8ac7f7db3de5499685e
Ravi <89218477+rshekhar-java@users.noreply.github.com>
2025-07-28 16:52:02 -0500

    Added User model and UserRepository with ReactiveMongoRepository (#1)
    Ready for merge

---
+++ user-service/src/main/java/com/rs/absbeauty/user/model/User.java
@@ -0,0 +1,73 @@
+package com.rs.absbeauty.user.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * created by rs 7/28/2025.
+ */
+@Document(collection = "users")
+public class User {
+
+    @Id
+    private String id;
+    private String username;
+    private String password;
+    private String email;
+    private List<String> roles;
+    private LocalDateTime createdAt=LocalDateTime.now();
+
+    // Getter and Setters
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
